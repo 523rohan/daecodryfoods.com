aec9f99e9ab2e5dbd345021307287f19dfa2966d
523rohan <rohangurav523.rg@gmail.com>
2026-02-08 11:14:22 +0530

    Show only lowest price on product listings instead of price range

--- resources/views/frontend/default/pages/partials/products/pricing.blade.php
+++ resources/views/frontend/default/pages/partials/products/pricing.blade.php
@@ -1,34 +1,18 @@
 @if (productBasePrice($product) == discountedProductBasePrice($product))
-    @if (productBasePrice($product) == productMaxPrice($product))
-        <span class="fw-bold h4 text-danger">{{ formatPrice(productBasePrice($product)) }}</span>
-    @else
-        <span class="fw-bold h4 text-danger">{{ formatPrice(productBasePrice($product)) }}</span>
-        -
-        <span class="fw-bold h4 text-danger">{{ formatPrice(productMaxPrice($product)) }}</span>
-    @endif
+    {{-- Show only base price, no range --}}
+    <span class="fw-bold h4 text-danger">{{ formatPrice(productBasePrice($product)) }}</span>
 @else
-    @if (discountedProductBasePrice($product) == discountedProductMaxPrice($product))
-        <span class="fw-bold h4 text-danger">{{ formatPrice(discountedProductBasePrice($product)) }}</span>
-    @else
-        <span class="fw-bold h4 text-danger">{{ formatPrice(discountedProductBasePrice($product)) }}</span>
-        -
-        <span class="fw-bold h4 text-danger">{{ formatPrice(discountedProductMaxPrice($product)) }}</span>
-    @endif
+    {{-- Show discounted base price only, no range --}}
+    <span class="fw-bold h4 text-danger">{{ formatPrice(discountedProductBasePrice($product)) }}</span>
 
     @if (isset($br))
         <br>
     @endif
 
     @if (!isset($onlyPrice) || $onlyPrice == false)
-        @if (productBasePrice($product) == productMaxPrice($product))
-            <span
-                class="fw-bold h4 deleted text-muted {{ isset($br) ? '' : 'ms-1' }}">{{ formatPrice(productBasePrice($product)) }}</span>
-        @else
-            <span
-                class="fw-bold h4 deleted text-muted {{ isset($br) ? '' : 'ms-1' }}">{{ formatPrice(productBasePrice($product)) }}</span>
-            -
-            <span class="fw-bold h4 deleted text-muted ms-1">{{ formatPrice(productMaxPrice($product)) }}</span>
-        @endif
+        {{-- Show original base price as strikethrough --}}
+        <span
+            class="fw-bold h4 deleted text-muted {{ isset($br) ? '' : 'ms-1' }}">{{ formatPrice(productBasePrice($product)) }}</span>
     @endif
 @endif
 

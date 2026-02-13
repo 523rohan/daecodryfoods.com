320811a87fffe3c35c86927443f37d2191ef4308
523rohan <rohangurav523.rg@gmail.com>
2026-02-13 19:35:13 +0530

    Remove background vegetable icons from feedback section

--- resources/views/frontend/default/pages/partials/home/feedback.blade.php
+++ resources/views/frontend/default/pages/partials/home/feedback.blade.php
@@ -3,16 +3,7 @@
         class="position-absolute start-0 bottom-0 z--1 w-100">
     <img src="{{ staticAsset('frontend/default/assets/img/shapes/map-bg.png') }}" alt="map"
         class="position-absolute start-50 top-50 translate-middle z--1">
-    <img src="{{ staticAsset('frontend/default/assets/img/shapes/fd-1.png') }}" alt="shape"
-        class="position-absolute z--1 fd-1">
-    <img src="{{ staticAsset('frontend/default/assets/img/shapes/fd-2.png') }}" alt="shape"
-        class="position-absolute z--1 fd-2">
-    <img src="{{ staticAsset('frontend/default/assets/img/shapes/fd-3.png') }}" alt="shape"
-        class="position-absolute z--1 fd-3">
-    <img src="{{ staticAsset('frontend/default/assets/img/shapes/fd-4.png') }}" alt="shape"
-        class="position-absolute z--1 fd-4">
-    <img src="{{ staticAsset('frontend/default/assets/img/shapes/fd-5.png') }}" alt="shape"
-        class="position-absolute z--1 fd-5">
+    {{-- Background shapes removed --}}
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-xl-6">
@@ -37,8 +28,7 @@
                             @foreach ($client_feedback as $feedback)
                                 <div class="swiper-slide feedback-single text-center">
                                     <p class="mb-5">{{ $feedback->review }}</p>
-                                    <span
-                                        class="clients_name text-dark fw-bold d-block mb-1">{{ $feedback->name }}</span>
+                                    <span class="clients_name text-dark fw-bold d-block mb-1">{{ $feedback->name }}</span>
                                     <ul class="star-rating fs-sm d-inline-flex align-items-center text-warning">
                                         {{ renderStarRatingFront($feedback->rating) }}
                                     </ul>

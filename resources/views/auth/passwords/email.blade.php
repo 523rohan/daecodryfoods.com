afbec1b0a97254a54f64c3e1e6f0cdeec4430dcd
523rohan <rohangurav523.rg@gmail.com>
2026-03-23 20:45:42 +0530

    'dd'

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -17,7 +17,7 @@
 
                         <div class="mb-7">
                             <a href="{{ route('home') }}">
-                                <img src="{{ uploadedAsset(getSetting('navbar_logo')) }}" alt="logo">
+                                <img src="{{ uploadedAsset(getSetting('navbar_logo')) }}" alt="logo" style="max-height: 120px; width: auto;">
                             </a>
                         </div>
                         <h2 class="mb-4 h3">{{ localize('Reset Password') }}

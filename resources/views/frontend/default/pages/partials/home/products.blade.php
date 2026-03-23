cf8380e33488197cc4c1a1e8f26dae19c654b201
523rohan <rohangurav523.rg@gmail.com>
2026-03-23 13:25:22 +0530

    'dd'

--- resources/views/frontend/default/pages/partials/home/products.blade.php
+++ resources/views/frontend/default/pages/partials/home/products.blade.php
@@ -1,4 +1,4 @@
-<section class="pt-80 pb-100">
+<section class="pt-60 pb-60">
     <div class="container">
         <div class="row justify-content-center justify-content-xl-between g-4">
             <div class="col-xl-9">
@@ -53,9 +53,10 @@
                 </div>
             </div>
             <div class="col-xl-3 d-none d-xl-block">
-                <a href="{{ getSetting('best_selling_banner_link') }}" class=""><img
-                        src="{{ uploadedAsset(getSetting('best_selling_banner')) }}" alt=""
-                        class="img-fluid rounded-2 d-flex flex-column h-100 object-fit-cover"></a>
+                <a href="{{ getSetting('best_selling_banner_link') }}" class="d-block h-100">
+                    <img src="{{ uploadedAsset(getSetting('best_selling_banner')) }}" alt=""
+                        class="img-fluid rounded-2 w-100 h-100 object-fit-cover">
+                </a>
             </div>
         </div>
     </div>
